4244539d9b1e788150ba1bbdf19f042ec4c1acdc
Ekin Ceylan <ekinceylan@gmail.com>
2025-06-13 23:03:02 +0300

    Create dom-helper.js

---
+++ dom-helper.js
@@ -0,0 +1,30 @@
+export const $ = selector => {
+  const elements = document.querySelectorAll(selector);  
+  elements.forEach(addExtensions);
+  
+  return elements.length > 1 ? elements : elements[0];
+};
+
+export const £ = tag => {
+  const element = document.createElement(tag);
+  
+  return addExtensions(element);
+};
+
+function addExtensions(el) {
+  el.addClass = function (...className) {
+    this.classList.add(...className);
+    return this;
+  };
+
+  el.attr = function (key, value) {
+    if (value) {
+      this.setAttribute(key, value);
+      return this;
+    }
+
+    return this.getAttribute(key);
+  };
+
+  return el;
+}
