bba1102eed22f8ab1945591e1127326f22a4a497
Ekin Ceylan <ekinceylan@gmail.com>
2026-04-10 16:25:57 +0300

    Enhance documentation with JSDoc comments for core functions in script.js

--- script.js
+++ script.js
@@ -1,51 +1,79 @@
-﻿function rulet(probs) {
+﻿/**
+ * Selects an index from a probability list using roulette-wheel selection.
+ * @param {number[]} probs - The probability weights.
+ * @returns {number} The selected index.
+ */
+function rulet(probs) {
     let total = sum(probs);
     let r = Math.random() * total;
 
-    let i = 0
+    let i = 0;
 
     for (; probs[i] < r; i++) {
         probs[i + 1] = probs[i + 1] + probs[i];
     }
 
-    return i
+    return i;
 }
 
-function dices(num){
+/**
+ * Rolls a die multiple times.
+ * @param {number} num - The number of dice to roll.
+ * @returns {number[]} An array containing the roll results.
+ */
+function dices(num) {
     let arr = [];
 
     for (let i = 0; i < num; i++) {
-        arr.push(dice());        
+        arr.push(dice());
     }
 
     return arr;
 }
 
-function dice(){
+/**
+ * Rolls a six-sided die once.
+ * @returns {number} A value between 1 and 6.
+ */
+function dice() {
     return rulet([1, 1, 1, 1, 1, 1]) + 1;
 }
 
-function sum(arr){
+/**
+ * Sums all numeric values in an array.
+ * @param {number[]} arr - The values to sum.
+ * @returns {number} The total sum.
+ */
+function sum(arr) {
     let total = 0;
 
     for (let i = 0; i < arr.length; i++) {
-        total += arr[i];        
+        total += arr[i];
     }
-    
+
     return total;
 }
 
+/**
+ * Creates a div element extended with helper methods.
+ * @returns {HTMLDivElement} The created div element.
+ */
 function div() {
     return addElementExtensions(document.createElement('DIV'));
 }
 
+/**
+ * Attaches helper methods directly to an element prototype.
+ * @param {Element} el - The element to extend.
+ * @returns {Element} The same extended element.
+ */
 function addElementExtensions(el) {
     //https://jaketrent.com/post/addremove-classes-raw-javascript/
     el.__proto__.addClass = function (className) {
         if (this.classList) {
             this.classList.add(className);
         } else if (!hasClass(this, className)) {
-            this.className += " " + className;
+            this.className += ' ' + className;
         }
 
         return this;
@@ -55,7 +83,7 @@ function addElementExtensions(el) {
         if (this.classList) {
             this.classList.remove(className);
         } else if (hasClass(el, className)) {
-            let reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+            let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
             this.className = this.className.replace(reg, ' ');
         }
 
@@ -65,13 +93,22 @@ function addElementExtensions(el) {
     return el;
 }
 
+/**
+ * Checks whether an element has the specified CSS class.
+ * @param {Element} el - The element to inspect.
+ * @param {string} className - The class name to find.
+ * @returns {boolean} True when the class exists.
+ */
 function hasClass(el, className) {
-    if (el.classList)
-        return el.classList.contains(className)
-    else
-        return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+    return el.classList ? el.classList.contains(className) : !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
 }
 
+/**
+ * Applies inline style rules to every element with the given class name.
+ * @param {string} className - The class name to query.
+ * @param {string} rules - Inline CSS rules to assign.
+ * @returns {void}
+ */
 function addStyleByClass(className, rules) {
     let elements = document.getElementsByClassName(className);
 
